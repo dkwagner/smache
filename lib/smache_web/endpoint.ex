65a5db80ff8ec42fef7f18c56370aaa86d0f6590
selfup <boudinot.regis@yahoo.com>
2019-05-05 12:06:58 -0500

    remove channel support - dev logs back in

--- lib/smache_web/endpoint.ex
+++ lib/smache_web/endpoint.ex
@@ -1,11 +1,6 @@
 defmodule SmacheWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :smache
 
-  socket("/socket", SmacheWeb.UserSocket,
-    websocket: true,
-    longpoll: [check_origin: false]
-  )
-
   if code_reloading? do
     plug(Phoenix.CodeReloader)
   end
